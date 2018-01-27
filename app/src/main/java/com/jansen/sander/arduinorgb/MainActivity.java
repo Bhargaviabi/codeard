7dc17a934a1f876a6bcff56df1545bf1c32eb286
Sander Jansen <sanderjansen1997@hotmail.com>
2018-01-27 15:31:31 +0100

    Toolbar on the dirty way

--- app/src/main/java/com/jansen/sander/arduinorgb/MainActivity.java
+++ app/src/main/java/com/jansen/sander/arduinorgb/MainActivity.java
@@ -10,19 +10,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.jansen.sander.arduinorgb.databinding.ActivityMainBinding;
 import com.jansen.sander.arduinorgb.databinding.ContentMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mainBinding;
+    //private ContentMainBinding contentMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ContentMainBinding contentMainBinding = DataBindingUtil.setContentView(this, R.layout.content_main);
-        //ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Inflate the content view (replacing `setContentView`)
+        //contentMainBinding = DataBindingUtil.setContentView(this, R.layout.content_main);
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Store the field now if you'd like without any need for casting
+        //TextView testView = mainBinding.textView2;
+
+
         //setContentView(R.layout.activity_main);
-        contentMainBinding.textView2.setText("test");
+        mainBinding.textView2.setText("test");
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -46,6 +57,10 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_colors) {
+            startActivity(new Intent(this, ColorActivity.class));
             return true;
         }
 
